d268a45af2162aa2903147d6154f696f44db98a3
acolombo09 <acolombo0911@gmail.com>
2023-06-30 15:15:42 +0200

    brainstorming_1

--- js/main.js
+++ js/main.js
@@ -11,29 +11,24 @@ L’output del prezzo finale va messo fuori in forma umana
 Questo richiederà un minimo di ricerca.
 */
 
-
 const firstName = prompt("Inserisci il tuo nome");
 const lastName = prompt("Inserisci il tuo cognome");
-const favColor = prompt("Inserisci il tuo colore preferito");
-const fixedNumber = 21;
+
 const age = prompt("Inserisci la tua età");
 
 document.getElementById("first-name").innerHTML = firstName;
 document.getElementById("last-name").innerHTML =  lastName;
-document.getElementById("fav-color").innerHTML =  favColor;
+
 
 document.getElementById("age").innerHTML = age;
 
-console.log(firstName + lastName + favColor + fixedNumber);
-const pswGenerated = (firstName + lastName + favColor + fixedNumber);
+console.log(firstName + lastName);
+const pswGenerated = (firstName + lastName);
 
 document.getElementById("psw-generated").innerHTML = pswGenerated;
 
 const bonusNumber = fixedNumber + parseInt((age));
 // document.getElementById("bonus-number").innerHTML = bonusNumber;
-
-const pswBonus = (firstName + lastName + favColor + bonusNumber);
-document.getElementById("psw-bonus").innerHTML = pswBonus;
 
 
 
